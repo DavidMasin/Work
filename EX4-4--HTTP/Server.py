3030de7785983733f01d2ba3df9a1c9dd5e99dcb
David Masin <davidmasin2910@gmail.com>
2023-10-01 16:21:50 +0300

    Finish 4.4

--- EX4-4--HTTP/Server.py
+++ EX4-4--HTTP/Server.py
@@ -1,61 +1,124 @@
+# Ex 4.4 - HTTP Server Shell
+# Author: Barak Gonen
+# Purpose: Provide a basis for Ex. 4.4
+# Note: The code is written in a simple way, without classes, log files or other utilities, for educational purpose
+# Usage: Fill the missing functions and constants
+import os.path
+# TO DO: import modules
 import socket
 
-IP = "127.0.0.1"
+# TO DO: set constants
+IP = '127.0.0.1'
 PORT = 80
-hasClient = False
+SOCKET_TIMEOUT = 6
 
 
-def connect():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((IP, PORT))
-    server_socket.listen()
-    print("Listening for connections on port %d" % PORT)
-    (client_socket, client_address) = server_socket.accept()
-    print("New connection received")
-    hasClient = True
-    return client_socket, client_address
-
-
-def disconnect(client_socket):
-    client_socket.close()
-    print("Connection terminated")
-    hasClient = False
-
-
-def getMessages(client_socket):
-    data = client_socket.recv(1024).decode()
-    if not ValidMessage(data):
-        print("Invalid message received:" + data)
-        disconnect(client_socket)
+def get_file_data(filename):
+    with open(filename, 'rb') as f:
+        data = f.read()
     return data
 
 
-def ValidMessage(data):
-    partsOfData = data.split(" ")
-    if partsOfData[0] == "GET" and partsOfData[2] == 'HTTP/1.1':
-        return True
-    return False
+def handle_client_request(resource, client_socket):
+    """ Check the required resource, generate proper HTTP response and send to client"""
+    # TO DO : add code that given a resource (URL and parameters) generates the proper response
+    DEFAULT_URL = '/80/index.html'
+
+    if resource == '':
+        url = DEFAULT_URL
+    else:
+        url = resource
+
+    # TO DO: check if URL had been redirected, not available or other error code. For example:
+    REDIRECTION_DICTIONARY = {'/': '/80/index.html', '/80/': '/80/index.html', '/index': '/80/index.html',
+                              '/home': '/80/index.html', '/80/home': '/80/index.html', '/default': '/80/index.html'}
+    if url in REDIRECTION_DICTIONARY:
+        url = REDIRECTION_DICTIONARY[url]
+        client_socket.send("HTTP/1.1 302 Found\r\n".encode())
+        # TO DO: send 302 redirection response
+    print(url)
+
+    # TO DO: extract requested file tupe from URL (html, jpg etc)
+    filetype = url.split('.')[-1]
+    print(filetype)
+    if filetype == 'html' or filetype == 'txt':
+        http_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"
+    elif filetype == 'jpg':
+        http_header = "HTTP/1.1 200 OK\r\nContent-Type: image/jpeg\r\n\r\n"
+    elif filetype == 'js':
+        http_header = "HTTP/1.1 200 OK\r\nContent-Type: text/javascript; charset=UTF-8\r\n\r\n"
+    elif filetype == 'css':
+        http_header = "HTTP/1.1 200 OK\r\nContent-Type: text/css\r\n\r\n"
+    else:
+        http_header = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n"
+    # TO DO: handle all other headers
+
+    # TO DO: read the data from the file
+    file_path = "C:\\Networks\\work\\EX4-4--HTTP\\webroot\\"
+
+    filename = url.replace('/80', '',1)
+    print("filename: " + filename)
+    if filename == './80/':
+        filename = 'index.html'
+    print(file_path + filename)
+    if (os.path.isfile(file_path + filename)):
+        data = get_file_data(file_path + filename)  # add file_path before filename
+        # print(data)
+    else:
+        http_header = "HTTP/1.1 404 Not Found\r\n\r\n"
+        data = b"<html><body><h1>404 Not Found</h1></body></html>"
+
+    http_response = http_header.encode() + data  # encode http_header before concatenating with data
+    print(http_header)
+    client_socket.send(http_response)
 
 
-def nameFile(data):
-    partsOfData = data.split(" ")
-    partOfURL = partsOfData[1].split("/")
-    return partOfURL[len(partOfURL) - 1]
+def validate_http_request(request):
+    # print(request)
+    realRequest = request.split("\r\n")[0]
+    partsOfRealRequest = realRequest.split(" ")
+    # print(partsOfRealRequest)
+    if len(partsOfRealRequest) != 3:
+        return False, None
+    if partsOfRealRequest[0] != 'GET' or partsOfRealRequest[2] != 'HTTP/1.1':
+        return False, None
+    return True, partsOfRealRequest[1]
 
 
-def sendFileName(client_socket, fileName):
-    client_socket.send(fileName.encode())
+def handle_client(client_socket):
+    """ Handles client requests: verifies client's requests are legal HTTP, calls function to handle the requests """
+    print('Client connected')
+    # client_socket.send("Hello".encode())
 
+    while True:
+        client_request = client_socket.recv(1024).decode()
+        valid_http, resource = validate_http_request(client_request)
+        if valid_http:
+            print('Got a valid HTTP request')
+            handle_client_request(resource, client_socket)
+            break
+        else:
+            client_socket.send("HTTP/1.1 500 Internal Server Error\r\n\r\n".encode())
+            print('Error: Not a valid HTTP request')
+            break
 
-def get_file_data(file_name):
-    with open(file_name, 'rb') as f:
-        return f.read()
+    print('Closing connection')
+    client_socket.close()
+
 
 def main():
-    client_socket, client_address = connect()
-    while hasClient:
-        data = getMessages(client_socket)
-        fileName = nameFile(data)
-        sendFileName(client_socket, fileName)
-        client_socket.send(get_file_data(fileName))
-    disconnect(client_socket)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, PORT))
+    server_socket.listen()
+    print(f"Listening for connections on port {PORT}")
+
+    while True:
+        client_socket, _ = server_socket.accept()
+        print('New connection received')
+        client_socket.settimeout(SOCKET_TIMEOUT)
+        handle_client(client_socket)
+
+
+if __name__ == "__main__":
+    # Call the main handler function
+    main()
