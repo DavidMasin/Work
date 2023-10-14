8dddb9304e49ff0839cfae00081b5e6a9deae2e0
David Masin <davidmasin2910@gmail.com>
2023-10-14 10:52:03 +0300

    1

--- Ex6_13_Secret_Communication/secret_message_server.py
+++ Ex6_13_Secret_Communication/secret_message_server.py
@@ -1,19 +1,22 @@
 from scapy.all import *
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, UDP
 
 secret_IP = "127.0.0.1"
-
 
 def getLetter(ASCII_num):
     return chr(ASCII_num)
 
 
 def filterMessage(packet):
-    return packet[IP].src == secret_IP
+    return UDP in packet and IP in packet and packet[IP].src == secret_IP
 
 
 def reciveMessage():
     while True:
-        packet = sniff(filter=filterMessage, count=1)
-        print(packet)
-        print(getLetter(packet[0][IP].dport), end="")
+        print("Waiting for message...")
+        my_packet = sniff(lfilter=filterMessage, count=1)
+        print(my_packet)
+        print(getLetter(my_packet[0][IP].dport), end="")
+
+if __name__ == '__main__':
+    reciveMessage()
